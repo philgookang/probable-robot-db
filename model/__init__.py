abc86ec0e05b1aaaf9071f2793ed8c0189c16270
Phil Goo Kang <philgookang@gmail.com>
2018-12-19 10:10:22 +0900

    added ProductImage Model to autoload

--- model/__init__.py
+++ model/__init__.py
@@ -2,3 +2,4 @@ from model.ProductBrandsM import ProductBrandsM
 from model.ProductCategoriesM import ProductCategoriesM
 from model.ProductColorsM import ProductColorsM
 from model.ProductsM import ProductsM
+from model.ProductImagesM import ProductImagesM
