594eb9e961450e9d8722a8e5bb6e130da631a028
Phil Goo Kang <philgookang@gmail.com>
2018-12-31 14:35:52 +0900

    added developer model to loader

--- model/__init__.py
+++ model/__init__.py
@@ -3,3 +3,4 @@ from model.ProductCategoriesM import ProductCategoriesM
 from model.ProductColorsM import ProductColorsM
 from model.ProductsM import ProductsM
 from model.ProductImagesM import ProductImagesM
+from model.DeveloperM import DeveloperM
